5bd8458581ac3d0bfb2b82e942724b1fa66e08b4
dthomas <dthomas@brandnetworksinc.com>
2013-12-16 15:41:21 -0500

    refrence this. instead of config

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 var config = {
     waitTime : 60000,
+    on: true,
     text: {
         NONE : 'None'
     },
     interval : function(){
         if(!$('#meh').hasClass('selected') && // If 'meh' is not selected
             !$('#woot').hasClass('selected') && // If 'woot' is not selected
-            $("#now-playing-dj .username").text() != config.text.NONE){ // If Current DJ is not 'None'
+            $("#now-playing-dj .username").text() != this.text.NONE){ // If Current DJ is not 'None'
                 $('#woot').click();
             }
-    }
+    },
 };
 
 setInterval(config.interval, config.waitTime);
