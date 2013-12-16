4a311ccf3d9172fe97fbab50c0934aa290908435
dthomas <dthomas@brandnetworksinc.com>
2013-12-16 13:42:10 -0500

    Refactored a bit. Only click 'woot' if 'meh' or 'woot' have not already been pushed.

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ var config = {
     waitTime : 60000,
     text: {
         NONE : 'None'
+    },
+    interval : function(){
+        if(!$('#meh').hasClass('selected') && // If 'meh' is not selected
+            !$('#woot').hasClass('selected') && // If 'woot' is not selected
+            $("#now-playing-dj .username").text() != config.text.NONE){ // If Current DJ is not 'None'
+                $('#woot').click();
+            }
     }
 };
 
-setInterval(function() {
-  if(!$('#meh').hasClass('selected') && $("#now-playing-dj .username").text() != config.text.NONE ) {
-    $('#woot').click();
-  }
-}, config.waitTime);
+setInterval(config.interval, config.waitTime);
 
 console.log("extension loaded!");
