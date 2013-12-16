dee29ec6a5bb4399fecd4442248773f11605b413
dthomas <dthomas@brandnetworksinc.com>
2013-12-16 15:57:30 -0500

    Watch out. I'm checking in commented code.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 var config = {
-    waitTime : 60000,
+    waitTime : 20000,
     on: true,
     text: {
         NONE : 'None'
     },
     interval : function(){
-        if(this.clickBotOn && // if ClickBot is on
-            !$('#meh').hasClass('selected') && // If 'meh' is not selected
+        console.log("Interval");
+        //if(this.clickBotOn && // if ClickBot is on
+        if(!$('#meh').hasClass('selected') && // If 'meh' is not selected
             !$('#woot').hasClass('selected') && // If 'woot' is not selected
             $("#now-playing-dj .username").text() != this.text.NONE){ // If Current DJ is not 'None'
-                if(this.alwaysClickWoot){
+                console.log("Clicked");
+                $('#woot').click();
+                /*if(this.alwaysClickWoot){
                     $('#woot').click();
                 }
                 else{
                     $('#meh').click();
-                }
+                }*/
             }
     },
     clickBotOn: true,   // Will Always Click woot or meh
