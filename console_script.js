763cd4b9757116bfa954de272e40af2933e657ea
dthomas <dthomas@brandnetworksinc.com>
2013-12-17 09:04:15 -0500

    Added a new file to run the script in the console. This will take advantage of plug.dj javascript API, until @MikeASchneider can figure out a way to access it from the chrome extension.

---
+++ console_script.js
@@ -0,0 +1,26 @@
+var config = {
+    waitTime : 60000,
+    on: true,
+    text: {
+        NONE : 'None'
+    },
+    interval : function(){
+        console.log("Interval");
+        if(config.on && // If bot is turned on
+            API.getUser().username != API.getDJ().username && // The logged in user is not the current DJ
+            !$('#meh').hasClass('selected') && // If 'meh' is not selected
+            !$('#woot').hasClass('selected') && // If 'woot' is not selected
+            $("#now-playing-dj .username").text() != config.text.NONE){ // If Current DJ is not 'None'
+                console.log("Clicked");
+                if(config.alwaysUpvote){
+                    $('#woot').click();
+                }
+                else{
+                    $('#meh').click();
+                }
+            }
+    },
+    alwaysUpvote: true
+};
+
+setInterval(config.interval, config.waitTime);
